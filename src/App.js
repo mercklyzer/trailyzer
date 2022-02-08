29232cef8cff6b8fca247ac6f8e8cc0f8ab05312
mercklyzer <mercklyzer@gmail.com>
2022-02-08 18:30:14 +0800

    Remove other Movie Rows

--- src/App.js
+++ src/App.js
@@ -33,11 +33,11 @@ function App() {
       {/* <NavigationBar query={query} setQuery={setQuery}/> */}
       <Banner />
 
-      {queryMoviesData.data && query !== '' && <MovieRow title={`Results for "${query}"`} movies={queryMoviesData.data} large={true}/>}
+      {/* {queryMoviesData.data && query !== '' && <MovieRow title={`Results for "${query}"`} movies={queryMoviesData.data} large={true}/>} */}
       {!popularMoviesData.loading && <MovieRow title="Popular Movies" movies={popularMoviesData.data} large={true}/>}
-      {!topRatedMoviesData.loading && <MovieRow title="Top Rated Movies" movies={topRatedMoviesData.data} large={true}/>}
-      {!trendingMoviesData.loading && <MovieRow title="Trending Movies" movies={trendingMoviesData.data} large={true}/>}
-      {!upcomingMoviesData.loading && <MovieRow title="Upcoming Movies" movies={upcomingMoviesData.data} large={true}/>}
+      {/* {!topRatedMoviesData.loading && <MovieRow title="Top Rated Movies" movies={topRatedMoviesData.data} large={true}/>} */}
+      {/* {!trendingMoviesData.loading && <MovieRow title="Trending Movies" movies={trendingMoviesData.data} large={true}/>} */}
+      {/* {!upcomingMoviesData.loading && <MovieRow title="Upcoming Movies" movies={upcomingMoviesData.data} large={true}/>} */}
 
     </div>
   );
